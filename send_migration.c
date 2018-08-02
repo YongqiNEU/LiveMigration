c018b4c99c658269f3dcf1b0ec65f20bd57be092
Varun Raval <vraval48@gmail.com>
2018-08-02 16:54:40 -0400

    formatted code

--- send_migration.c
+++ send_migration.c
@@ -1,19 +1,21 @@
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <netinet/in.h>
 #include <string.h>
 
-#include <sys/socket.h>
 #include "migration_header.h"
+#include <sys/socket.h>
 #define PORT 5000
 
-int main(int argc, char const *argv[]) {
+int
+main(int argc, char const* argv[])
+{
   struct sockaddr_in address;
   int sock = 0, valread;
   struct sockaddr_in serv_addr;
 
-  char buffer[1024] = {0};
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+  char buffer[1024] = { 0 };
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("\n Socket creation error \n");
     return -1;
   }
@@ -22,35 +24,34 @@ int main(int argc, char const *argv[]) {
 
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
-  
-  if(inet_pton(AF_INET, "10.110.176.160", &serv_addr.sin_addr)<=0) {
+
+  if (inet_pton(AF_INET, "10.110.176.160", &serv_addr.sin_addr) <= 0) {
     printf("\nInvalid address/ Address not supported \n");
     return -1;
   }
 
-  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+  if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
     printf("\nConnection Failed \n");
     return -1;
   }
 
-
-  // sending 
+  // sending
   struct stat file_stat;
   int fd = open("readonly", O_RDONLY);
-  while (fd == -1){
+  while (fd == -1) {
     fd = open("readonly", O_RDONLY);
   }
-  //Get file stats
-  if (fstat(fd, &file_stat) < 0){
-      fprintf(stderr, "Error fstat --> %s", strerror(errno));
+  // Get file stats
+  if (fstat(fd, &file_stat) < 0) {
+    fprintf(stderr, "Error fstat --> %s", strerror(errno));
   }
 
   int offset = 0;
   int remain_data = file_stat.st_size;
   /* Sending file data */
-  while (((sent_bytes = sendfile(sock,fd , &offset, BUFSIZ)) > 0) && (remain_data > 0))
-  {
-      remain_data -= sent_bytes;
+  while (((sent_bytes = sendfile(sock, fd, &offset, BUFSIZ)) > 0) &&
+         (remain_data > 0)) {
+    remain_data -= sent_bytes;
   }
 
   return 0;
--- send_migration_helper.c
+++ send_migration_helper.c
@@ -1,141 +1,160 @@
+#include <ctype.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ucontext.h>
-#include <signal.h>
 #include <string.h>
 #include <sys/mman.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/types.h>
+#include <ucontext.h>
+#include <unistd.h>
 
-void signalHandler(int signal);
-void savingCheckPointImage();
-void parseSectionHeader(char * header, struct memorySection * section);
-int readLine(int checkpoint_image_fd, char line[256]);
-void writeToImage(int fd, struct memorySection * section);
+void
+signalHandler(int signal);
+void
+savingCheckPointImage();
+void
+parseSectionHeader(char* header, struct memorySection* section);
+int
+readLine(int checkpoint_image_fd, char line[256]);
+void
+writeToImage(int fd, struct memorySection* section);
 
 /////*****************************************************************/////
 
 // constructor SIGUSR2 signal and signal handler
 // this constructor can be called before main()
-__attribute__ ((constructor))
-void myconstructor() {
-    signal(SIGUSR2, signalHandler);
+__attribute__((constructor)) void
+myconstructor()
+{
+  signal(SIGUSR2, signalHandler);
 }
 
 // signal handler for signal SIGUSR2
 // it will initiate saving a check point image file
-void signalHandler(int signal){ 
-    if(signal == SIGUSR2){
-        savingCheckPointImage();
-    }
+void
+signalHandler(int signal)
+{
+  if (signal == SIGUSR2) {
+    savingCheckPointImage();
+  }
 }
 
 /////*****************************************************************/////
 
-// saving a checkpoint image file   
-void savingCheckPointImage() {
-    int pid = getpid();
-    char line[256];
+// saving a checkpoint image file
+void
+savingCheckPointImage()
+{
+  int pid = getpid();
+  char line[256];
 
-    //context for saving and restoring registers
-    ucontext_t context;
+  // context for saving and restoring registers
+  ucontext_t context;
 
-    //read memory layout
-    int memory_layout_fd = open("/proc/self/maps", O_RDONLY);
-    // create a new image file for saving the checkpoint
-    int checkpoint_image_fd = open("readonly", O_CREAT | O_RDWR, S_IRWXU);
+  // read memory layout
+  int memory_layout_fd = open("/proc/self/maps", O_RDONLY);
+  // create a new image file for saving the checkpoint
+  int checkpoint_image_fd = open("readonly", O_CREAT | O_RDWR, S_IRWXU);
 
-    // number of memory sections
-    int counter = 0;
-    write(checkpoint_image_fd, &counter, sizeof(int)); 
+  // number of memory sections
+  int counter = 0;
+  write(checkpoint_image_fd, &counter, sizeof(int));
 
-    // parse memory sections
-    struct memorySection section;
-    while (readLine(memory_layout_fd, line) > 0) {
-        parseSectionHeader(line, &section);
-        // skip vsyscall, vvar and vdso lines
-        if (section.permissions[0] == 'r' && 
-            section.permissions[1] != 'w' &&
-        	strstr(line, "vvar") == NULL && strstr(line, "vdso") == NULL &&
-        	strstr(line, "vsyscall") == NULL) {
+  // parse memory sections
+  struct memorySection section;
+  while (readLine(memory_layout_fd, line) > 0) {
+    parseSectionHeader(line, &section);
+    // skip vsyscall, vvar and vdso lines
+    if (section.permissions[0] == 'r' && section.permissions[1] != 'w' &&
+        strstr(line, "vvar") == NULL && strstr(line, "vdso") == NULL &&
+        strstr(line, "vsyscall") == NULL) {
 
-            writeToImage(checkpoint_image_fd, &section);
+      writeToImage(checkpoint_image_fd, &section);
 
-            counter++;
-        }
+      counter++;
     }
-    // save counter
-    lseek(checkpoint_image_fd, 0, SEEK_SET);
-    write(checkpoint_image_fd, &counter, sizeof(int));
+  }
+  // save counter
+  lseek(checkpoint_image_fd, 0, SEEK_SET);
+  write(checkpoint_image_fd, &counter, sizeof(int));
 
-    close(memory_layout_fd);
+  close(memory_layout_fd);
+  close(checkpoint_image_fd);
+
+  getcontext(&context);
+  // save context
+  if (pid == getpid()) {
+    // read checkpoint image file
+    checkpoint_image_fd = open("myckpt", O_RDWR);
+    lseek(checkpoint_image_fd, 0, SEEK_END);
+
+    int context_ret = write(checkpoint_image_fd, &context, sizeof(context));
+    if (context_ret != sizeof(context)) {
+      printf("context failed to write\n");
+    }
+
     close(checkpoint_image_fd);
+  } else {
+    printf("Program is restored\n");
 
-    getcontext(&context);
-    // save context
-    if (pid == getpid()) {
-        // read checkpoint image file
-        checkpoint_image_fd = open("myckpt", O_RDWR);
-        lseek(checkpoint_image_fd, 0, SEEK_END);
-
-        int context_ret = write(checkpoint_image_fd, &context, sizeof(context));
-		if(context_ret != sizeof(context)){
-    		printf("context failed to write\n");
-    	}
-
-        close(checkpoint_image_fd);
-    } else {
-        printf("Program is restored\n");
-
-                ////to implement userfaultfd
-        ///////
-    }
+    ////to implement userfaultfd
+    ///////
+  }
 }
-
 
 /////*****************************************************************/////
 ////////////////////////// helper functions //////////////////////////////
 
-int readLine(int checkpoint_image_fd, char line[256]) {
-    char c = 0;
-    int len = 0;
+int
+readLine(int checkpoint_image_fd, char line[256])
+{
+  char c = 0;
+  int len = 0;
 
-    while(len == 0 || c != '\n'){
-        if(read(checkpoint_image_fd, &c, 1) == 0) {
-        	//printf("Reach the end of the file \n");
-        	return len;
-        }
+  while (len == 0 || c != '\n') {
+    if (read(checkpoint_image_fd, &c, 1) == 0) {
+      // printf("Reach the end of the file \n");
+      return len;
+    }
 
-        *line = c;
-        line += 1;
-        len += 1;
-    } 
+    *line = c;
+    line += 1;
+    len += 1;
+  }
 
-    *line = '\0';
-    return len;
+  *line = '\0';
+  return len;
 }
 
 // write memory seections to destination fd;
-void writeToImage(int fd , struct memorySection * section){
-	ssize_t ret = write(fd, section, sizeof(struct memorySection));
-    if(ret != sizeof(struct memorySection)){
-       	printf("Section write failed \n");
-    }
+void
+writeToImage(int fd, struct memorySection* section)
+{
+  ssize_t ret = write(fd, section, sizeof(struct memorySection));
+  if (ret != sizeof(struct memorySection)) {
+    printf("Section write failed \n");
+  }
 
-    ret = write(fd, section->start, section->end - section->start);
-    if(ret != section->end-section->start){
-    	printf("Section data failed to write\n");
-    }
+  ret = write(fd, section->start, section->end - section->start);
+  if (ret != section->end - section->start) {
+    printf("Section data failed to write\n");
+  }
 }
 
 // parse a section header from a memory section
 // store info into memory section structure
-void parseSectionHeader(char * header, struct memorySection * section) {
-    sscanf(header, "%p-%p %c%c%c%c %x",
-           &section->start, &section->end,
-           &section->permissions[0], &section->permissions[1], 
-           &section->permissions[2], &section->permissions[3], &section->offset);
+void
+parseSectionHeader(char* header, struct memorySection* section)
+{
+  sscanf(header,
+         "%p-%p %c%c%c%c %x",
+         &section->start,
+         &section->end,
+         &section->permissions[0],
+         &section->permissions[1],
+         &section->permissions[2],
+         &section->permissions[3],
+         &section->offset);
 }
