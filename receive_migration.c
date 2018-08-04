9d710ee14257b9134f029a7367137f44ab5e40ad
Varun Raval <vraval48@gmail.com>
2018-08-04 13:27:51 -0400

    mmapped memory pages and saved their contents

--- receive_migration.c
+++ receive_migration.c
@@ -2,23 +2,43 @@
 
 #include <arpa/inet.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <poll.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include "migration_header.h"
+
+char CHECKPOINT_PATH[256] = "process_checkpoint.ckpt";
 
 short
 Poll(int, int);
 
 void
-ReadPagesContext(int, int*, struct memorySection*);
+ReadPagesContext(int, int*);
+
+void*
+MapStack(void*, size_t);
+
+struct memorySection*
+GetStackMemorySection();
+
+void
+RestoreMemory(int);
+
+void* CreateMmap(struct memorySection);
 
 ucontext_t context;
+
+struct memorySection stackMemoryRegion;
 
 int
 main(int argc, char* argv[])
@@ -31,9 +51,10 @@ main(int argc, char* argv[])
   struct sockaddr_in clientAddr;
   socklen_t clientAddrLen;
 
-  struct memorySection *readMemorySections, *temp;
+  struct memorySection* temp;
 
   VA stackStartAddr = 0x5300000;
+  void *stackEndAddr, *stackAssignedAddr;
   size_t stackSize;
 
   if (argc < 3) {
@@ -47,28 +68,55 @@ main(int argc, char* argv[])
   sockFd = WaitForMigration(
     ipAddr, portNo, (struct sockaddr*)&clientAddr, &clientAddrLen);
 
-  ReadPagesContext(sockFd, &numReadPages, readMemorySections);
+  // TODO get address of canCheckpoint variable also
+  ReadPagesContext(sockFd, &numReadPages);
 
   temp = GetStackMemorySection();
   if (temp) {
-    stackSize = temp->endAddr - temp->startAddr;
+    stackSize = temp->end - temp->start;
     stackMemoryRegion = *temp;
   } else {
     ShowError("No original stack (Unknown error)", 0);
   }
 
   stackAssignedAddr = MapStack((void*)stackStartAddr, stackSize);
+  if (stackAssignedAddr) {
+    stackEndAddr = stackAssignedAddr + stackSize;
+
+    asm volatile("mov %0,%%rsp" : : "g"(stackEndAddr) : "memory");
+    RestoreMemory(numReadPages);
+  }
+
+  return 0;
 }
 
 void
-ReadPagesContext(int sockFd,
-                 int* numReadPages,
-                 struct memorySection* readMemorySections)
+ReadPagesContext(int sockFd, int* numReadPages)
 {
   int i, ret;
   short events;
+  size_t tempSize;
 
-  for (i = 0; i < 3; i++) {
+  struct memorySection* temp;
+  void* buf;
+
+  int fd = open(CHECKPOINT_PATH, O_RDWR | O_CREAT);
+
+  events = Poll(sockFd, -1);
+  if (events == -1) {
+    ShowError("", errno);
+  }
+
+  if (events & (POLLHUP | POLLRDHUP)) {
+    ShowError("connection lost", 0);
+  }
+
+  ret = read(sockFd, (void*)numReadPages, sizeof(int));
+  if (ret == -1) {
+    ShowError("", errno);
+  }
+
+  for (i = 0; i <= *numReadPages; i++) {
     events = Poll(sockFd, -1);
     if (events == -1) {
       ShowError("", errno);
@@ -80,20 +128,32 @@ ReadPagesContext(int sockFd,
 
     if (events & POLLIN) {
       // read data
-      if (i == 0) {
-        ret = read(sockFd, (void*)numReadPages, sizeof(int));
-        if (ret == -1) {
-          ShowError("", errno);
-        }
-      } else if (i == 1) {
-        readMemorySections =
-          malloc(sizeof(struct memorySection) * *numReadPages);
+      if (i < *numReadPages) {
+        if (i % 2 == 0) {
+          temp = malloc(sizeof(struct memorySection));
 
-        ret = read(sockFd,
-                   (void*)readMemorySections,
-                   sizeof(struct memorySection) * *numReadPages);
-        if (ret == -1) {
-          ShowError("", errno);
+          ret = read(sockFd, (void*)temp, sizeof(struct memorySection));
+          if (ret == -1) {
+            ShowError("", errno);
+          }
+
+          ret = write(fd, (void*)temp, sizeof(struct memorySection));
+          if (ret == -1) {
+            ShowError("", errno);
+          }
+        } else {
+          tempSize = temp->end - temp->start;
+          buf = malloc(tempSize);
+
+          ret = read(sockFd, buf, tempSize);
+          if (ret == -1) {
+            ShowError("", errno);
+          }
+
+          ret = write(fd, buf, tempSize);
+          if (ret == -1) {
+            ShowError("", errno);
+          }
         }
       } else {
         ret = read(sockFd, (void*)&context, sizeof(ucontext_t));
@@ -105,6 +165,8 @@ ReadPagesContext(int sockFd,
       ShowError("data expected but no data obtained", 0);
     }
   }
+
+  close(fd);
 }
 
 short
@@ -149,25 +211,101 @@ GetStackMemorySection()
 {
   char fileName[21], line[256], *name;
   int pid, ret;
+  char stackFound = 0;
 
   pid = getpid();
   sprintf(fileName, "/proc/%d/maps", pid);
 
   int fileReadFd = open(fileName, O_RDONLY);
+  if (fileReadFd == -1) {
+    ShowError("", errno);
+  }
 
-  struct memorySection mem;
+  struct memorySection* mem = malloc(sizeof(struct memorySection));
   while (1) {
     ret = readLine(fileReadFd, line);
     if (ret <= 0)
       break;
 
-    parseSectionHeader(line, &mem);
-    name = getNameFromOffset(mem->offset);
+    parseSectionHeader(line, mem);
+    name = getNameFromSectionLine(line);
     if (name && !strcmp(name, "[stack]")) {
+      stackFound = 1;
       break;
     }
   }
 
   close(fileReadFd);
-  return mem;
+  return stackFound ? mem : NULL;
+}
+
+void
+RestoreMemory(int numReadPages)
+{
+  int i;
+  struct memorySection mem;
+
+  void* mapped;
+
+  int fd, ret;
+
+  fd = open(CHECKPOINT_PATH, O_RDONLY);
+  if (fd == -1) {
+    ShowError("", errno);
+  }
+
+  // unmap old stack
+  ret = munmap((void*)stackMemoryRegion.start,
+               stackMemoryRegion.end - stackMemoryRegion.start);
+  if (ret == -1) {
+    ShowError("", errno);
+  }
+
+  for (i = 0; i < numReadPages; i++) {
+    ret = read(fd, &mem, sizeof(mem));
+    if (ret == -1) {
+      ShowError("", errno);
+    }
+
+    // mapping memory region
+    mapped = CreateMmap(mem);
+    if (mapped == MAP_FAILED) {
+      printf("map failed\n");
+      continue;
+    }
+
+    // reading address block and writing to appropriate address
+    ret = read(fd, mapped, mem.end - mem.start);
+    if (ret == -1) {
+      ShowError("", errno);
+    }
+  }
+
+  close(fd);
+}
+
+// creates mapping in the virtual memory using mem struct and
+// returns: the start address of the mapping
+void*
+CreateMmap(struct memorySection mem)
+{
+  int prot = 0, flags = 0;
+
+  if (mem.permissions[0] == 'r')
+    prot |= PROT_READ;
+
+  // if (mem.isWriteable)
+  prot |= PROT_WRITE;
+
+  if (mem.permissions[2] == 'x')
+    prot |= PROT_EXEC;
+
+  if (mem.permissions[3] == 'p')
+    flags = MAP_PRIVATE;
+  else
+    flags = MAP_SHARED;
+
+  flags |= MAP_ANONYMOUS;
+
+  return mmap((void*)(mem.start), mem.end - mem.start, prot, flags, -1, 0);
 }
